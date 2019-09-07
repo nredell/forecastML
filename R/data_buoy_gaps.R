86f53635aef9a2de53bd21eeef993cded396daac
Nick Redell <nickredell@hotmail.com>
2019-09-07 00:12:15 -0500

    Added examples and docs for fill_gaps

---
+++ R/data_buoy_gaps.R
@@ -0,0 +1,22 @@
+#' NOAA buoy weather data
+#'
+#' A dataset containing daily average sensor measurements of several environmental
+#' conditions collected by 14 buoys in Lake Michigan from 2012 through 2018. This
+#' dataset is identical to the \link{data_buoy} dataset except that there are gaps in
+#' the daily sensor data. Running \code{fill_gaps()} on \code{data_buoy_gaps} will
+#' produce \code{data_buoy}.
+#'
+#' @format A data.frame with 23,646 rows and 9 columns:
+#' \describe{
+#'   \item{date}{date}
+#'   \item{wind_spd}{average daily wind speed in kts}
+#'   \item{buoy_id}{the station ID for each buoy}
+#'   \item{lat}{latitude}
+#'   \item{lon}{longitude}
+#'   \item{day}{day of year}
+#'   \item{year}{calendar year}
+#'   \item{air_temperature}{air temperature in degrees Fahrenheit}
+#'   \item{sea_surface_temperature}{water temperature in degrees Fahrenheit}
+#' }
+#' @source \url{http://www.ndbc.noaa.gov/}
+"data_buoy"
--- R/examples/example_fill_gaps.R
+++ R/examples/example_fill_gaps.R
@@ -1,29 +1,19 @@
 \donttest{
-# Sampled Seatbelts data from the R package datasets.
-data("data_seatbelts", package = "forecastML")
-#------------------------------------------------------------------------------
-# Example 1 - Training data for 12 horizon-specific models w/ common lags per predictor.
-horizons <- 1:12
-lookback <- 1:15
 
-data <- data_seatbelts
+  # NOAA buoy dataset with gaps in data collection
+  data("data_buoy_gaps", package = "forecastML")
 
-data_train <- create_lagged_df(data_seatbelts, type = "train", outcome_cols = 1,
-                               lookback = lookback, horizon = horizons)
-head(data_train[[length(horizons)]])
+  data_buoy_no_gaps <- fill_gaps(data_buoy_gaps, date_col = 1, frequency = '1 day',
+                                 groups = 'buoy_id', static_features = c('lat', 'lon'))
 
-# Example 1 - Forecasting dataset
-# The last 'nrow(data_seatbelts) - horizon' rows are automatically used from data_seatbelts.
-data_forecast <- create_lagged_df(data_seatbelts, type = "forecast", outcome_cols = 1,
-                                  lookback = lookback, horizon = horizons)
-head(data_forecast[[length(horizons)]])
-#------------------------------------------------------------------------------
-# Example 2 - Training data for one 3-month horizon model w/ unique lags per predictor.
-horizons <- 3
-lookback <- list(c(3, 6, 9, 12), c(4:12), c(6:15), c(8))
+  # The returned data.frame has the same number of columns but the time-series are now evenly spaced
+  # at 1 day apart. Additionally, the unchanging grouping columns and static features columns have
+  # been filled in for the newly created dataset rows.
+  dim(data_buoy_gaps)
+  dim(data_buoy_no_gaps)
 
-data_train <- create_lagged_df(data_seatbelts, type = "train", outcome_cols = 1,
-                               lookback_control = lookback, horizon = horizons)
-head(data_train[[length(horizons)]])
-#------------------------------------------------------------------------------
+  # Running create_lagged_df() is the next step in the forecastML forecasting process. If there are long
+  # gaps in data collection, like in this buoy dataset, and the user-supplied modeling algorithm
+  # cannot handle missing outcomes data, the best option is to filter these rows out in the user-supplied
+  # modeling function for train_model()
 }
