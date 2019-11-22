1bc05157659067231d87299b0887df7931bf5434
Nick Redell <nickredell@hotmail.com>
2019-11-22 02:00:22 -0600

    Add unit tests for create_windows

--- tests/testthat/test_create_windows.R
+++ tests/testthat/test_create_windows.R
@@ -17,7 +17,7 @@ test_that("create_windows with dates is correct", {
   # create_lagged_df(data_test) should equal the constructed data.
   data_test <- data
 
-  lookback = 1:12
+  lookback <- 1:12
 
   data_test <- forecastML::create_lagged_df(data_test, horizons = 3, lookback = lookback,
                                             dates = dates, frequency = "1 month")
@@ -28,7 +28,33 @@ test_that("create_windows with dates is correct", {
 
   identical(windows$start, windows$stop)
 })
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 
+test_that("create_windows with the same start and stop date for 1 unit of time is correct", {
+
+  # Create a simple data.frame with 1 feature.
+  dates <- seq(as.Date("2015-01-01"), as.Date("2020-12-01"), by = "1 month")
+
+  data <- data.frame(
+    "outcome" = 1:length(dates),
+    "feature" = 1:length(dates) * 2
+  )
+
+  # create_lagged_df(data_test) should equal the constructed data.
+  data_test <- data
+
+  lookback <- 1:12
+
+  data_test <- forecastML::create_lagged_df(data_test, horizons = 3, lookback = lookback,
+                                            dates = dates, frequency = "1 month")
+
+  window_length <- 1
+
+  windows <- forecastML::create_windows(data_test, window_length = window_length)
+
+  identical(windows$start, windows$stop)
+})
 #------------------------------------------------------------------------------
 #------------------------------------------------------------------------------
 
@@ -45,19 +71,19 @@ test_that("create_windows length and skip with dates is correct", {
   # create_lagged_df(data_test) should equal the constructed data.
   data_test <- data
 
-  lookback = 1:12
+  lookback <- 1:12
 
   data_test <- forecastML::create_lagged_df(data_test, horizons = 3, lookback = lookback,
                                             dates = dates, frequency = "1 month")
 
-  window_length <- 3  # A 3-month window.
+  window_start_true <- as.Date(c("2017-01-01", '2019-05-01'))
+  window_stop_true <- as.Date(c("2018-03-01", '2019-07-01'))
 
-  skip <- 2  # Skip 2 months between windows.
-
-  windows <- forecastML::create_windows(data_test, window_length = window_length, skip = skip)
+  windows <- forecastML::create_windows(data_test, window_start = window_start_true,
+                                        window_stop = window_stop_true)
 
   all(
-    windows$stop[1] %m-% months(window_length - 1) == windows$start[1],  # check window length
-    windows$start[2] %m-% months(skip + 1) == windows$stop[1]            # check skip
+    all(windows$start == window_start_true),
+    all(windows$stop == window_stop_true)
   )
 })
