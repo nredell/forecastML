4bf9de526ec2255fd224410766ff616a96b088ac
Nick Redell <nickredell@hotmail.com>
2020-04-09 12:02:33 -0500

    Fix unit tests for create_skeleton

--- tests/testthat/test_create_skeleton.R
+++ tests/testthat/test_create_skeleton.R
@@ -7,13 +7,13 @@ test_that("create_skeleton correctly preserves lagged_df objects", {
 
   data("data_seatbelts", package = "forecastML")
 
-  data_lagged <- create_lagged_df(data_seatbelts, "train", "direct", horizons = 1:3,
-                                  lookback = 1:5)
+  data_lagged <- create_lagged_df(data_seatbelts, "train", "direct", horizons = 1:2,
+                                  lookback = 1:3)
 
   data_skeleton <- create_skeleton(data_lagged)
 
   data_lagged_attr <- attributes(data_lagged)
-  data_skeleton_attr <- attributes(data_skeleton)[!attributes(data_skeleton_attr)$names %in% "skeleton"]
+  data_skeleton_attr <- attributes(data_skeleton)[!names(attributes(data_skeleton)) %in% "skeleton"]
 
   identical(data_lagged_attr, data_skeleton_attr)
 })
