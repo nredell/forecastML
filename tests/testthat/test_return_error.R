85605a5067a13e3550a03cff773a96318273919d
Nick Redell <nickredell@hotmail.com>
2020-04-20 11:13:46 -0500

    Add unit tests for final forecast error metrics

--- tests/testthat/test_return_error.R
+++ tests/testthat/test_return_error.R
@@ -372,3 +372,72 @@ test_that("return_error returns correctly when the input is 1 model from combine
    nrow(data_error_combined[[2]]) == max(horizons)
   )
 })
+
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+
+test_that("error metric calculations are correct for combine_forecasts(type = 'horizon')", {
+
+  data("data_seatbelts", package = "forecastML")
+
+  horizons <- c(3, 12)
+  lookback <- 1:15
+
+  data_train <- create_lagged_df(data_seatbelts, type = "train", outcome_col = 1,
+                                 lookback = lookback, horizon = horizons)
+
+  windows <- create_windows(data_train, window_length = 0)
+
+  historical_mean <- round(mean(data_seatbelts$DriversKilled), 0)
+
+  model_function <- function(data) {
+    model <- historical_mean
+    return(model)
+  }
+
+  set.seed(224)
+  model_results <- train_model(data_train, windows, model_name = "mean", model_function)
+
+  data_forecast <- create_lagged_df(data_seatbelts, type = "forecast", outcome_col = 1,
+                                    lookback = lookback, horizon = horizons)
+
+  prediction_function <- function(model, data_features) {
+    data_pred <- data.frame("y_pred" = model)
+    return(data_pred)
+  }
+
+  data_forecasts <- predict(model_results, prediction_function = list(prediction_function), data = data_forecast)
+
+  data_combined <- combine_forecasts(data_forecasts)
+
+  set.seed(224)
+  data_test <- data.frame("DriversKilled" = runif(12, 110, 160), "index" = 193:(193 + 11))
+  test_indices <- data_test$index
+
+  data_error_combined <- return_error(data_combined, data_test, test_indices)
+
+  # Manual calculations
+  mae_global <- mean(abs(data_test$DriversKilled - data_combined$DriversKilled_pred))
+  rmse_global <- sqrt(mean((data_test$DriversKilled - data_combined$DriversKilled_pred)^2))
+  mape_global <- mean(abs(data_test$DriversKilled - data_combined$DriversKilled_pred) / abs(data_test$DriversKilled)) * 100
+
+  # Calculate error by forecast horizon.
+  data_error_horizon <- lapply(1:max(horizons), function(i) {
+
+    mae <- mean(abs(data_test$DriversKilled[i] - data_combined$DriversKilled_pred[i]))
+    rmse <- sqrt(mean((data_test$DriversKilled[i] - data_combined$DriversKilled_pred[i])^2))
+    mape <- mean(abs(data_test$DriversKilled[i] - data_combined$DriversKilled_pred[i]) / abs(data_test$DriversKilled[i])) * 100
+    data.frame(mae, rmse, mape)
+  })
+
+  data_error_horizon <- dplyr::bind_rows(data_error_horizon)
+
+  all(
+    identical(mae_global, data_error_combined$error_global$mae),
+    identical(rmse_global, data_error_combined$error_global$rmse),
+    identical(mape_global, data_error_combined$error_global$mape),
+    identical(data_error_horizon, data_error_combined$error_by_horizon[, c("mae", "rmse", "mape")])
+  )
+})
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
